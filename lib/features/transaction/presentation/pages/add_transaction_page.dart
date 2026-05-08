e26fab6bf7dabc54d4d82f54076b71126e32cf5e
attaryu <mattarannaufal@gmail.com>
2026-05-09 02:24:20 +0700

    fix(transaction): reset add transaction form
    - made a reset method in `AddTransactionCubit` and call it to reset error state from server
    - reorder form reset call in the last to clearing all flag and error state

--- lib/features/transaction/presentation/pages/add_transaction_page.dart
+++ lib/features/transaction/presentation/pages/add_transaction_page.dart
@@ -254,23 +254,22 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                         onChanged: (value) {
                           setState(() {
                             _selectedTransactionType = value;
-                            
-                            // 6. Reset Validasi
-                            _formKey.currentState?.reset();
-                            
+
+                            context.read<AddTransactionCubit>().reset();
+
                             // 1. Reset Nominal
                             _amountController.clear();
-                            
+
                             // 2. Reset Judul
                             _nameController.clear();
-                            
+
                             // 3. Reset Catatan
                             _noteController.clear();
-                            
+
                             // 4. Reset Tanggal ke hari ini
                             _selectedDate = DateTime.now();
                             _dateController.text = _formatDate(_selectedDate);
-                            
+
                             // 5. Reset Kategori ke default awal tipe tersebut
                             _selectedCategory = 0;
                             if (value == TransactionType.expense) {
@@ -282,6 +281,9 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                                 _selectedCategory = _incomeCategories.first.id;
                               }
                             }
+
+                            // 6. Reset Validasi
+                            _formKey.currentState?.reset();
                           });
                         },
                       ),
